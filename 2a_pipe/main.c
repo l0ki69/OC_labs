ed0510cf68b13399390860bfdf892eddd53b89bd
l0ki69 <48375711+l0ki69@users.noreply.github.com>
2020-12-15 12:53:29 +0300

    update

--- 2a_pipe/main.c
+++ 2a_pipe/main.c
@@ -16,24 +16,36 @@ int main()
 	time_t ttime;
 
 	pid_t pid = fork();
-	if(pid == 0){	//CHILD - source
-		printf("CHILD: put sys time in pipe\n");
+	if(pid == 0)  //CHILD - source
+    {	
+	    //sleep(1);
+		printf("CHILD: get sys time from pipe\n");
+
+		close(fd[1]);
+		long int Mas[2] = {0, 0};
+		read(fd[0], Mas, sizeof(long int) * 2);
+		time_t timer = time(0);
+		printf("CHILD:\nchild_time = %s", ctime(&timer));
+		timer = Mas[0];
+		printf("parent_time = %s", ctime(&timer));
+		printf("parent_pid = %ld\n", Mas[1]);
+
+		exit(0);
+	}
+    else  //PARENT - target
+    {
+		printf("PARENT: put sys time in pipe\n");
 
 		close(fd[0]);
-		time_t data = time(0);
-		write(fd[1], &data, sizeof(time_t));
+		long int mas[2];
+		mas[0] = time(0);
+		mas[1] = getpid();
+		sleep(5);
+		write(fd[1], mas , sizeof(long int) * 2);
 		close(fd[1]);
 
-		printf("CHILD: process terminated\n");
-		exit(0);
-	}else{		//PARENT - target
-		sleep(1);
-		printf("PARENT: get sys time from pipe\n");
-
-		close(fd[1]);
-		time_t buf;
-		read(fd[0], &buf, sizeof(time_t));
-		printf("PARENT: %s\n", ctime(&buf));
+		printf("PARENT: process terminated\n");
 	}
+
 	return 0;
 }

fb88ff86ad35e45084e7f5ce4e9471242196b916
l0ki69 <48375711+l0ki69@users.noreply.github.com>
2020-12-15 20:06:28 +0300

    lab complite

--- laba_3/in.c
+++ laba_3/in.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <signal.h>
 
+#define FILE "out.c"
+
+void funcExit(int sig)
+{
+    printf("\n");
+    exit(0);
+}
+
+int main()
+{
+    signal(SIGINT, funcExit);
+
+	char * addr;
+	int mem;
+	if( (mem= (shmget(2002, 32, IPC_EXCL))) == -1 )
+    {
+        printf("SM open eror\n");
+        exit(0);
+    }
+
+    if((addr = shmat( mem, NULL, 0 )) == (char*)-1)
+    {
+		printf("SM link error\n");
+        exit(0);
+    }
+
+    while(1)
+    {   
+        time_t timer = time(0);
+        printf("\ntime_Consumer = %spid_Consumer = %d\n", ctime(&timer), getpid());
+        printf("\n%s", addr);
+        sleep(1);
+    }
+    return 0;
+}
---
+++ laba_3/out.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+
+
+#define FILE "Makefile"
+
+int shmid;
+
+void funcExit(int sig)
+{
+    printf("\n");
+    struct shmid_ds *buf = 0;
+    shmctl(shmid, IPC_RMID, buf);
+
+    exit(0);
+}
+
+int main()
+{
+    signal(SIGINT, funcExit);
+    char * addr;
+    shmid = (shmget(2002, 32, IPC_CREAT | 0666));
+
+    if( shmid == -1 )
+    {
+        printf("Can't create shared memory\n");
+        exit(0);
+    }
+
+    if((addr = shmat( shmid, NULL, 0 )) == (char*)-1)
+    {
+            printf("Shmat err\n");
+			exit(0);
+	}
+
+    time_t timer = time(0);
+    time_t buft = timer;
+
+    if (strlen(addr) != 0)
+    {
+        printf("there is already a sending process\n");
+        exit(0);
+    }
+
+    while(1)
+    {
+        timer = time(0);
+        if (timer != buft)
+        {
+            buft = timer;
+            sprintf(addr, "time_Producer = %spid_Producer = %d\n", ctime(&timer), getpid());
+        }
+    }
+    return 0;
+}

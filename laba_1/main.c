3426dc276f35c5e782ef50f5c9b6f1fd6be39389
l0ki69 <48375711+l0ki69@users.noreply.github.com>
2020-12-16 16:47:21 +0300

    Add files via upload

--- laba_1/main.c
+++ laba_1/main.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
 
+void funcExit(){
+	printf("atexit in proccess with pid = %d\n",getpid());
+	return;
+}
+
+int main()
+{
+    pid_t pid = fork();
+    atexit(funcExit);
+
+    if (pid == 0) //CHILD
+    {
+        printf("CHILD:\nchild_pid = %d\nchild_ppid = %d\n", getpid(), getppid());
+        exit(0);
+    }
+    else //PARENT
+    {
+        wait(NULL);
+    }
+
+    printf("PARENT:\nparent_pid = %d\nparent_ppid = %d\n", getpid(), getppid());
+
+    return 0;
+}

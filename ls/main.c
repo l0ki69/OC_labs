642f46ffe0c4c628e6c120edd22d4606567ec69f
l0ki69 <48375711+l0ki69@users.noreply.github.com>
2020-12-17 21:51:37 +0300

    ls -ls fix

--- ls/main.c
+++ ls/main.c
@@ -1,47 +1,68 @@
-#include <stdlib.h>
-#include <dirent.h> 
+#include <stdio.h>
 #include <unistd.h>
-#include <stdio.h> 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
 #include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
-int main(void)
+int myls(struct dirent * dirr)
 {
-	DIR *d;
-	int n = 0;
-	struct dirent *dir = (struct dirent*) calloc (sizeof (struct dirent)+1, 1);
-	struct dirent **namelist;
-	struct stat *buff;
-	time_t t;
-	buff = (struct stat*) calloc (sizeof(struct stat), 1);
-	n = scandir (".", &namelist, NULL, alphasort);
-	int i=0;
-	while (i<n)
+	char * path_buff;
+	path_buff = calloc(sizeof(char)*(strlen(dirr->d_name)) + 2, sizeof(char));
+	path_buff[0] = '.';
+	path_buff[1] = '/';
+	strcpy(&(path_buff[2]), dirr->d_name);
+
+	struct stat S;
+	memset(&S, 0, sizeof(struct stat));
+	stat(path_buff, &S);
+	printf((S.st_mode & S_IFDIR)?"d":"-");
+	printf((S.st_mode & S_IRUSR)?"r":"-");
+	printf((S.st_mode & S_IWUSR)?"w":"-");
+	printf((S.st_mode & S_IXUSR)?"x":"-");
+	printf((S.st_mode & S_IRGRP)?"r":"-");
+	printf((S.st_mode & S_IWGRP)?"w":"-");
+	printf((S.st_mode & S_IXGRP)?"x":"-");
+	printf((S.st_mode & S_IROTH)?"r":"-");
+	printf((S.st_mode & S_IWOTH)?"w":"-");
+	printf((S.st_mode & S_IXOTH)?"x":"-");
+	printf(" %ld", S.st_nlink);
+	struct passwd * pwd;
+	pwd = getpwuid(S.st_uid);
+	printf(" %s", pwd->pw_name);
+	struct group * grp;
+	grp = getgrgid(S.st_gid);
+	printf(" %s", grp->gr_name);
+	printf(" %5d ", (int)S.st_size);
+	struct tm * time_ptr;
+	time_t It = S.st_ctime;
+	time_ptr = localtime(&It);
+	printf(" %02d.%02d", time_ptr->tm_mday, time_ptr->tm_mon+1);
+	printf(" %02d:%02d ", time_ptr->tm_hour, time_ptr->tm_min);
+	printf("%s", dirr->d_name);
+	printf("\n");
+	
+	return 0;
+}
+
+int main()
+{
+	int n;
+	struct dirent ** entry;
+	if(-1 == (n = scandir("./", &entry, NULL, alphasort)))
+		return 1;
+	else
 	{
-		dir = namelist[i];
-		stat (dir->d_name, buff);
-		
-		printf ((buff->st_mode & S_IFDIR)?"d":"-");
-		printf ((buff->st_mode & S_IRUSR)?"r":"-");
-		printf ((buff->st_mode & S_IWUSR)?"w":"-");
-		printf ((buff->st_mode & S_IXUSR)?"x":"-");
-		printf ((buff->st_mode & S_IRGRP)?"r":"-");
-		printf ((buff->st_mode & S_IWGRP)?"w":"-");
-		printf ((buff->st_mode & S_IXGRP)?"x":"-");
-		printf ((buff->st_mode & S_IROTH)?"r":"-");
-		printf ((buff->st_mode & S_IWOTH)?"w":"-");
-		printf ((buff->st_mode & S_IXOTH)?"x":"-");
-		
-		char mtime[80];
-		t = buff->st_mtime;
-		struct tm lt;
-		localtime_r(&t, &lt);
-		strftime(mtime, sizeof mtime, "%d %b %Y %T", &lt);
-		printf ("\t%s", mtime);
-		printf ("\t%5lld\t %s\n", buff->st_size, dir->d_name);
-		free (namelist[n]);
-		i++;
+		//
 	}
-	return(0);
+	printf("%d files in directory\n", n);
+	int dir_size = n-1;
+	while(n--){
+		myls(entry[dir_size - n]);
+	}
+	return 0;
 }

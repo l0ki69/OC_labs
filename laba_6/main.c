34451ff840af0e0bfc84abda55ed8cf13c432c43
l0ki69 <48375711+l0ki69@users.noreply.github.com>
2020-12-18 15:27:25 +0300

    Блокировка чтения-записи

--- laba_6/main.c
+++ laba_6/main.c
@@ -1,96 +1,76 @@
+#include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <sys/ipc.h>
-#include <time.h>
-#include <sys/types.h>
-#include <pthread.h>
 #include <sys/shm.h>
-#include <signal.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-#define FILE "shared_memory"
+#define READ_THREAD_COUNT 10
 
 pthread_rwlock_t lock;
 
-int shmid;
+int count = 0;
 
-void funcExit(int sig)
+void funcExit(int sig) 
 {
-    printf("\n");
-    struct shmid_ds *buf = 0;
-    shmctl(shmid, IPC_RMID, buf);
-
-    exit(0);
+	printf("\n");
+	exit(0);
 }
 
-void* read_func()
+void *read_func(void *num) 
 {
-	key_t key = ftok(FILE, 'a');
-	shmid = shmget (key, 32, IPC_EXCL | 0666);
-
-    if (shmid == -1)
-    {
-    	printf ("Shared memory creation failed\n");
-        return 0;
-    }
-
-	char* addr = shmat (shmid, NULL, 0);
-	if (addr == (char*)-1)
-    {
-		printf ("Shmat failed\n");
-		return 0;
-	}
-
-	while (1)
-    {
-         pthread_rwlock_rdlock (&lock);
-		printf ("%lx %s\n", pthread_self(), addr);
-		pthread_rwlock_unlock (&lock);
-		sleep(1);
-	}
-
-	return 0;
-}
-
-int main()
-{
-    signal(SIGINT, funcExit);
-
-    key_t key = ftok(FILE, 'a');
-	int smid = shmget (key, 32, IPC_CREAT | 0666);
-
-	pthread_t pth[10];
-
-	 pthread_rwlock_init (&lock, NULL);
-
-    for (int i = 0; i < 10; i++)
-    {   
-	    pthread_create (&pth[i], NULL, read_func, NULL);
-    }
-
-	if (smid == -1)
+	while (1) 
 	{
-		printf ("Shared memory creation failed\n");
-		return 0;
+    	pthread_rwlock_rdlock (&lock);
+    	printf("Я %d-й поток, мой tid =  %lx, счетчик  = %d\n", *(int *)num,
+           	(long)pthread_self(), count);
+    	fflush(stdout);
+    	pthread_rwlock_unlock (&lock);
+		sleep(1);
+  	}
+}
+
+void *write_func(void *arg) 
+{
+	while (1) 
+	{
+		sleep(rand() % 10);
+		pthread_rwlock_wrlock(&lock);
+		count++;
+		if (count > 10000)
+		count = 0;
+		printf("Я увеличил счетчик до значения %d\n", count);
+		printf("...and then relax...");
+		fflush(stdout);
+		sleep(rand() % 10);
+		pthread_rwlock_unlock(&lock);
+  	}
+}
+
+int main() {
+	//  Thread initialization
+	signal(SIGINT, funcExit);
+
+	pthread_t pth[READ_THREAD_COUNT + 1];
+	int thread_nums[READ_THREAD_COUNT];
+
+	pthread_rwlock_init (&lock, NULL);
+
+	for (int i = 0; i < READ_THREAD_COUNT; i++) 
+	{
+    	thread_nums[i] = i;
+    	pthread_create(&pth[i], NULL, read_func, &thread_nums[i]);
 	}
 
-	char* addr = shmat (smid, NULL, 0);
-	if (addr == (char*)-1)
-    {
-		printf ("Shmat failed\n");
-		return 0;
-	}
+	pthread_create(&pth[READ_THREAD_COUNT], NULL, write_func, NULL);
 
-    int count = 0;
-
-	while (1)
-    {
-        pthread_rwlock_wrlock(&lock);
-       	sprintf(addr, "%d", count);
-        pthread_rwlock_unlock(&lock);
-		sleep (1);
-        count++;
+	for (int i = 0; i < READ_THREAD_COUNT + 1; i++) 
+	{
+    	pthread_join(pth[i], NULL);
 	}
 
 	pthread_rwlock_destroy(&lock);

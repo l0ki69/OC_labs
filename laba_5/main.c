3392f3a8d2d3c7a3afcb1b4148da4bb1725bf3ee
l0ki69 <48375711+l0ki69@users.noreply.github.com>
2020-12-18 15:21:38 +0300

    mutex

--- laba_5/main.c
+++ laba_5/main.c
@@ -1,75 +1,76 @@
+#include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <sys/ipc.h>
-#include <time.h>
-#include <sys/types.h>
-#include <pthread.h>
 #include <sys/shm.h>
-#include <signal.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
+#define READ_THREAD_COUNT 10
 pthread_mutex_t mutex;
 
 int count = 0;
 
-void funcExit(int sig)
+void funcExit(int sig) 
 {
-    printf("\n");
-    exit(0);
+  printf("\n");
+  exit(0);
 }
 
-void* read_func(int num)
+void *read_func(void *num) 
 {
-	while (1)
-    {
-        pthread_mutex_lock (&mutex);
-		printf ("Я %d-й поток, мой tid =  %lx, счетчик  = %d\n", num , pthread_self(), count);
-		pthread_mutex_unlock(&mutex);
-		sleep(1);
-	}
-
-	return 0;
+  while (1) 
+  {
+    pthread_mutex_lock(&mutex);
+    printf("Я %d-й поток, мой tid =  %lx, счетчик  = %d\n", *(int *)num,
+           (long)pthread_self(), count);
+    fflush(stdout);
+    pthread_mutex_unlock(&mutex);
+	sleep(1);
+  }
 }
 
-void* write_func()
+void *write_func(void *arg) 
 {
-	while (1)
-	{   
-		pthread_mutex_unlock(&mutex);
-		count++;
-		printf("Я увеличил счетчик до значения %d\n", count);
-		sleep(rand() % 10);
-		pthread_mutex_lock(&mutex);
-		if (count > 10000) count = 0;
-	}
+  while (1) 
+  {
+    sleep(rand() % 10);
+    pthread_mutex_lock(&mutex);
+    count++;
+    if (count > 10000)
+      count = 0;
+    printf("Я увеличил счетчик до значения %d\n", count);
+    printf("...and then relax...");
+  	fflush(stdout);
+    sleep(rand() % 10);
+    pthread_mutex_unlock(&mutex);
+  }
 }
 
-int main()
-{
-	//  Thread initialization
-    signal(SIGINT, funcExit);
+int main() {
+  //  Thread initialization
+  signal(SIGINT, funcExit);
 
-	pthread_t pth[11];
+  pthread_t pth[READ_THREAD_COUNT + 1];
+  int thread_nums[READ_THREAD_COUNT];
+  pthread_mutex_init(&mutex, NULL);
 
-	pthread_mutex_init (&mutex, NULL);
+  for (int i = 0; i < READ_THREAD_COUNT; i++) 
+  {
+    thread_nums[i] = i;
+    pthread_create(&pth[i], NULL, read_func, &thread_nums[i]);
+  }
 
-	fsync(&stdout);
+  pthread_create(&pth[READ_THREAD_COUNT], NULL, write_func, NULL);
 
-    for (int i = 0; i < 10; i++)
-    {   
-		int num = i + 1;
-	    pthread_create (&pth[i], NULL, read_func, num);
-    }
+  for (int i = 0; i < READ_THREAD_COUNT + 1; i++) 
+  {
+    pthread_join(pth[i], NULL);
+  }
 
-	pthread_create(&pth[10], NULL, write_func, NULL);
-
-	for (int i = 0; i < 11; i++)
-	{
-		pthread_join(&pth[i], NULL);
-	}
-
-
-	pthread_mutex_destroy(&mutex);
-	return 0;
+  pthread_mutex_destroy(&mutex);
+  return 0;
 }
